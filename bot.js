8981356068096e0f28246e857e52970d37e64ec5
Sparks989 <89668312+Sparks989@users.noreply.github.com>
2021-11-02 12:30:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,8 +49,42 @@ function retweetLatest() {
 	});
 }
 
-// Try to retweet something as soon as we run the program...
-retweetLatest();
+// Replies to a top rated tweet with edited text.
+function replyToTopWEditText() {
+	T.get('explore/tabs/trending', {count:5}, function (error, data) {
+	  // log out any errors and responses
+	  console.log(error, data);
+	  // If our search request to the server had no errors...
+	  var tweetText = data.statuses[0].text;
+	  for(var i = 0; i < tweetText.length; i++){
+	  	if(Math.random() > 0.5){
+	  		tweetText[i] = tweetText[i].toUpperCase;
+	  	}
+	  }
+	  if(Math.random() > 0.5) {
+	  	tweetText = tweetText + "?";
+	  }
+	  if (!error) {
+	  	// ...then we grab the ID of the tweet we want to retweet...
+		var retweetId = data.statuses[0].id_str;
+		// ...and then we tell Twitter we want to retweet it!
+		T.post('statuses/retweet/' + retweetId, {statuses = tweetText }, function (error, response) {
+			if (response) {
+				console.log('Success! Check your bot, it should have retweeted something.')
+			}
+			// If there was an error with our Twitter call, we print it out here.
+			if (error) {
+				console.log('There was an error with Twitter:', error);
+			}
+		})
+	  }
+	  // However, if our original search request had an error, we want to print it out here.
+	  else {
+	  	console.log('There was an error with your hashtag search:', error);
+	  }
+	});
+
+}
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetLatest, 1000 * 60 * 60);
