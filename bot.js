15c320bb2a03d4923376d3168277951ed1790e6f
Sparks989 <89668312+Sparks989@users.noreply.github.com>
2021-11-05 13:31:16 -0400

    editText implementation

--- bot.js
+++ bot.js
@@ -50,8 +50,12 @@ function retweetLatest() {
 }
 
 // Replies to a top rated tweet with edited text.
-function replyToTopWEditText() {
-	T.get('explore/tabs/trending', {count:5}, function (error, data) {
+
+
+
+
+function editText() {
+	T.get('explore/tabs/trending', {count:5}, );
 	  // log out any errors and responses
 	  console.log(error, data);
 	  // If our search request to the server had no errors...
@@ -64,27 +68,36 @@ function replyToTopWEditText() {
 	  if(Math.random() > 0.5) {
 	  	tweetText = tweetText + "?";
 	  }
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, {statuses = tweetText }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
+	  function tweetIt(tweetText) {
+		var tweet = {
+			status: tweetText
+		}
 
+		T.post('statuses/update', tweet, tweeted);
+
+		function tweeted(err, data, response) {
+			if (err) {
+				console.log("Something went wrong!");
+			} else {
+				console.log("It worked!");
+			}
+		}
+	}
+	  
 }
-// ...and then every hour after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+
+function tweetIt(txt) {
+	var tweet = {
+		status: txt
+	}
+
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+		if (err) {
+			console.log("Something went wrong!");
+		} else {
+			console.log("It worked!");
+		}
+	}
+}
