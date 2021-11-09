7fd60b61fb4cee2b46c9111df04eafa54d32d0ee
XByDesign <90591097+Xbydesign1@users.noreply.github.com>
2021-11-08 20:13:41 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-// Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'Nn33lka7ahOhkn0TXVecqnAIh',
-  consumer_secret:      'Ot25SOtsNEIByJgnitRPPrfhlr6MCgqgYUmqvz3DzznIUT3rgq',
-  access_token:         '1452657257829834758-FrD1qybDYkXuOlw0L5SIIrCZD5x0gB',
-  access_token_secret:  'NVP6ned0NZwmSjbDJRHFmo2DvC80spaKxmyQCLiztxlzE'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
 }
