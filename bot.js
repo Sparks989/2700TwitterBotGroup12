8c2760728c39cf6b040fe58d42ae11acadd8e572
GSlaw <46406693+GSlaw@users.noreply.github.com>
2021-11-08 20:26:20 -0500

    FINAL EDITS
    - Cleanup
    - Comments
    - Auto Run Enable

--- bot.js
+++ bot.js
@@ -1,3 +1,11 @@
+/**
+ * LMC 2700
+ * ASSIGNMENT 5 - TWITTERBOT
+ * GROUP 12
+ * @version 1.0
+ */
+
+
 // DEBUG
 var debug = false;
 
@@ -7,37 +15,7 @@ var Twit = require('twit');
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
-
-// This function finds the latest teet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + editText(retweetId), { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
-}
-
-
+// List of communities to repost in so our bot is not constantly spamming the same one.
 var climateSearch = {q: "#climate", count: 1, result_type: "recent"};
 var politicsSearch = {q: "#politics", count: 1, result_type: "recent"};
 var techSearch = {q: "#tech", count: 1, result_type: "recent"};
@@ -45,42 +23,60 @@ var wildlifeSearch = {q: "#wildlife", count: 1, result_type: "recent"};
 
 var paramList = [climateSearch, politicsSearch, techSearch, wildlifeSearch];
 
-// Function that simply makes sure the code is compling without issue.
+
+/**
+ * Function that simply makes sure the code is compling without issue.
+ */
 function test() {
 	let Str = 'test';
 	console.log(Str);
 }
 
-// Replies to a popular tweet with edited text.
+
+/**
+ * Function that retweets a recent tweet from the paramList of communities.
+ * Calls editText() and tweetIt() to edit the tweet text and post.
+ */
 function reTweet() {
 	T.get('search/tweets',paramList[Math.floor(Math.random()*4)] , function(error,data,response) {
-		if(response) {
-			console.log('The getTweet() function worked!')
-		} else if(error) {
-			console.log('Back to the drawing board.')
-		}
 		tweetIt(editText(data.statuses[0].text));
+		if (response) {
+			console.log('\nSuccess! Check your bot, it should have retweeted something.\n')
+		} else if (error) {
+			console.log('\nBack to the drawing board.\n', error)
+		} else {
+			console.log('\nThere was an error with your hashtag search:\n', error);
+		}
 	});
 }
 
-// Function modifies a string of text, capitalizing
-// every even char and adding a '? ;)'. Returns the new string.
+
+/**
+ * Function modifies a string of text, capitalizing
+ * every even char and adding a '? ;)' at the end.
+ * @param tweetText - text taken in to be modified.
+ * @returns a new modified string.
+*/
 function editText(tweetText) {
 	let newTweet = '';
 	i = 0;
 	while (i < tweetText.length) {
-		if (i % 2 == 0) {
+		if (i % 2 == 0) { // Makes even chars uppercase
 			newTweet = newTweet + tweetText[i].toUpperCase();
 		} else {
-			newTweet = newTweet + tweetText[i];
+			newTweet = newTweet + tweetText[i]; // Leaves odd chars alone
 		}
 		i++;
 	}
-	newTweet = newTweet + '? ;)';
+	newTweet = newTweet + '? ;)'; // Adds a final flourish to the end of every retweet
 	return newTweet;
 }
 
-// Function that tweets some text on command.
+
+/**
+ * Function that posts a new  some text on command.
+ * @param txt - a string that will be posted to Twitter.
+*/
 function tweetIt(txt) {
 	var tweet = {
 		status: txt
@@ -89,18 +85,33 @@ function tweetIt(txt) {
 	function tweeted(err, data, response) {
 		if (err) {
 			console.log("Something went wrong in tweetIt()!");
-		} else {
-			console.log("The tweetIt() function worked!");
 		}
 	}
 }
 
+
+/**
+ * Function that automates our bot's retweeting.
+*/
+function runBot() {
+	console.log('GT Bot Running...'); // Incation that things are happening
+	console.log(" "); // For legible logs
+	var d = new Date();
+	var ds = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+	console.log(ds);  // date/time of the request	
+
+	// Bot Logic
+	var rand = Math.random();
+
+ 	if (rand <= 1.60) {      
+		console.log("\nIncoming ReTweet...");
+		reTweet();
+	}
+}
+
+
 // Run the bot
-//test();
-//getTweet();
-//console.log(editText('helloooooooo'));
-//retweetLatest();
-//tweetIt('Hello world!');
-reTweet();
-// And recycle every hour
-//setInterval(runBot, 1000 * 60 * 60);
+runBot();
+
+// Recycles every hour
+setInterval(runBot, 1000 * 60 * 60);
