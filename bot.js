512b254b2a95b1df3613fffd93e379a92a42119d
yadu1002 <83854011+yadu1002@users.noreply.github.com>
2021-11-10 17:30:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,16 +23,16 @@ var T = new Twit(require('./config.js'));
 var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
 
 // This function finds the latest teet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
+function retweetLatest = () => {
+	T.get('search/tweets', mediaArtsSearch, function (error, data) => {
 	  // log out any errors and responses
-	  console.log(error, data);
+	  console.log(error, data),
 	  // If our search request to the server had no errors...
 	  if (!error) {
 	  	// ...then we grab the ID of the tweet we want to retweet...
 		var retweetId = data.statuses[0].id_str;
 		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+		T.post('statuses/retweet/' + retweetId, {}, (error, response) => {
 			if (response) {
 				console.log('Success! Check your bot, it should have retweeted something.')
 			}
@@ -41,9 +41,8 @@ function retweetLatest() {
 				console.log('There was an error with Twitter:', error);
 			}
 		})
-	  }
+	  } else {
 	  // However, if our original search request had an error, we want to print it out here.
-	  else {
 	  	console.log('There was an error with your hashtag search:', error);
 	  }
 	});
@@ -54,3 +53,60 @@ retweetLatest();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetLatest, 1000 * 60 * 60);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+var stream = T.stream('user');
+
+//This particular stream function detects whether someone has followed your bot or has mentioned it anywhere.
+stream._on('follow', followed);
+stream._on('tweet', tweetEvent);
+
+function followed(eventMessage) {
+	var name = eventMessage.source.name;
+	var screenName = eventMessage.source.screen_name;
+	tweetStatus('@' + screenName + ' Thank you for following me!');
+}
+//The tweetEvent() function detects whether our username was mentioned anywhere on Twitter and replies back.
+function tweetEvent(tweetMSG) {
+	var fs = request('fs');
+	var json = JSON.stringify(tweetMSG, null, 2);
+	fs.writeFile("tweets.json", json);
+
+	var replyTo = tweet.om_reply_to_screem_name;
+	var text = tweetMSG.text;
+	var from = tweetMSG.user.screen_name;
+
+	console.log(replyTo + from);
+	console.log(text);
+	console.log();
+
+	if (replyTo == 'Tweets4retweets') {
+		var newTweet = ('@' + from + " Thank you for mentioning me! #MediumRocks");
+		tweetStatus(newTweet);
+	}
+}
+
+//im trying to do code the following the people part...sorry about the delay
+function follow() {
+	T.get('statuses/mentions_timeline', { count:50, include_rts:1 },  function (err, reply) {
+		  if (err !== null) {
+			console.log('Error: ', err);
+		 } else {
+		  	var sn = reply.pick().user.screen_name;
+			if (debug) {
+				console.log(sn);
+		    } else {
+				//Now follow that user
+				T.post('friendships/create', {screen_name: sn }, function (err, reply) {
+					if (err !== null) {
+						console.log('Error: ', err);
+					} else {
+						console.log('Followed: ' + sn);
+					}
+				});
+			}
+		}
+	});
+}
